e962338e6b2755927fe27939b722e1ccd4d4b775
Nathan C <me@nathanc.tech>
2024-12-30 16:46:49 -0500

    Added new workflow

---
+++ scripts/setupToml.sh
@@ -0,0 +1,24 @@
+echo "> Deleting old"
+rm -rf repo/testing/live/${INTERNAL_NAME}
+echo "> Making new"
+if [[ ${MESSAGE} =~ .*"[TEST]".* ]]; then
+    mkdir repo/testing/live/${INTERNAL_NAME}
+    cd repo/testing/live/${INTERNAL_NAME}
+else
+    rm -rf repo/stable/${INTERNAL_NAME}
+    mkdir repo/stable/${INTERNAL_NAME}
+    cd repo/stable/${INTERNAL_NAME}
+fi
+echo "[plugin]" >>manifest.toml
+echo "repository = \"${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}.git\"" >>manifest.toml
+echo "owners = [ \"${GITHUB_REPOSITORY_OWNER}\" ]" >>manifest.toml
+echo "project_path = \"${INTERNAL_NAME}\"" >>manifest.toml
+echo "commit = \"${GITHUB_SHA}\"" >>manifest.toml
+echo "changelog = ${CHANGELOG}" >>manifest.toml
+echo "version = \"${VERSION}\"" >>manifest.toml
+echo "> Done"
+cat manifest.toml
+mkdir images
+cd images
+echo "> Downloading images"
+curl -L "https://raw.githubusercontent.com/Era-FFXIV/ShopItemRevealer/main/icon.png" -o icon.png
